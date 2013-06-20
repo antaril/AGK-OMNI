6ff431ed7e07ce9e3ae648575ebe2bf40a463747
maxwen <max.weninger@gmail.com>
2013-06-20 03:03:23 +0200

    sched: added time-based average load
    Conflicts:
    	include/linux/sched.h
    	kernel/sched/sched.h
    
    Conflicts:
    	kernel/sched/sched.h

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -119,6 +119,8 @@ extern unsigned long nr_iowait_cpu(int cpu);
 extern unsigned long this_cpu_load(void);
 
 extern void sched_get_nr_running_avg(int *avg, int *iowait_avg);
+extern unsigned long get_avg_nr_running(unsigned int cpu);
+extern unsigned long avg_nr_running(void);
 
 extern void calc_global_load(unsigned long ticks);
 
